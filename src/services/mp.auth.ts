638df9e5f887fedd927f49296557cebd3e91f53e
dillon-courts <dillon.courts@gmail.com>
2019-05-07 07:20:59 -0400

    Fixing signout bugs

--- src/services/mp.auth.ts
+++ src/services/mp.auth.ts
@@ -1,14 +1,15 @@
 import { Observable, of } from "rxjs";
 
 import { MpTokens } from "../models/crds-token.mp";
+import { Utilities } from "../utility/cookie";
 
 export class CrdsMpService {
   constructor(private accessTokenCookie: string, private refreshTokenCookie: string) { }
 
   public authenticated(): Observable<MpTokens> {
     // Just check for the presence of a cookie
-    let accessToken = this.getCookie(this.accessTokenCookie);
-    let refreshToken = this.getCookie(this.refreshTokenCookie);
+    let accessToken = Utilities.getCookie(this.accessTokenCookie);
+    let refreshToken = Utilities.getCookie(this.refreshTokenCookie);
 
     if (accessToken)
       return of(MpTokens.From({ access_token: accessToken, refresh_token: refreshToken }));
@@ -16,34 +17,15 @@ export class CrdsMpService {
       return of(null);
   }
 
-  private getCookie(cname) {
-    var name = cname + '=';
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return '';
-  }
-
-  private deleteCookie(cname: string)
-  {
-    document.cookie = cname + '=; Max-Age=-99999999;';
-  }
-
   public signOut(): Observable<boolean> {
-    let accessToken = this.getCookie(this.accessTokenCookie);
+    let accessToken = Utilities.getCookie(this.accessTokenCookie);
 
     if (accessToken) {
       // clear both the access and refresh tokens
-      this.deleteCookie(this.accessTokenCookie);
-      this.deleteCookie(this.refreshTokenCookie);
+      Utilities.deleteCookie(this.accessTokenCookie, '.crossroads.net');
+      Utilities.deleteCookie(this.refreshTokenCookie, '.crossroads.net');
+
+      return of(true);
     }
     else {
       return of(false);
