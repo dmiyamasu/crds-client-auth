47de0148e87c2082f61fb2b28362d7b04ee0b3ef
dillon-courts <dillon.courts@gmail.com>
2019-05-07 11:15:38 -0400

    Exporting ICRDSTokens, sample test file

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@
 // ...
 export { CrdsAuthenticationService } from './services/crds.auth';
 export { CRDSAuthConfig, OktaConfig, MpConfig } from './models/crds-auth-config.model';
+export { ICRDSTokens } from './models/crds-token.interface';
 export { LoggerService } from './services/logger.service';
 export { AuthenticationProviders } from './models/crds-token.interface';
